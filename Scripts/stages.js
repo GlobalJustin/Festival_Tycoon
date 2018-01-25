25200f60bfbec5326c48b301a8fede7b993c9276
David Gregory <dpgregory>
2018-01-25 17:11:23 +0000

    stages.js

---
+++ Scripts/stages.js
@@ -0,0 +1,32 @@
+var stages = [
+    {
+        "name": "The Arena",
+        "x": 390,
+        "y": 284
+    },
+    {
+        "name": "The Racket Club",
+        "x": 944,
+        "y": 489
+    },
+    {
+        "name": "The Street Party",
+        "x": 656,
+        "y": 373
+    },
+    {
+        "name": "The Forest Club",
+        "x": 512,
+        "y": 627
+    },
+    {
+        "name": "The Fun Hous",
+        "x": 893,
+        "y": 410
+    },
+    {
+        "name": "The Bruck'n Stadl",
+        "x": 92,
+        "y": 371
+    }
+]
